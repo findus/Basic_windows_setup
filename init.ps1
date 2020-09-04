3bdd41431dd9334dad9f981f5989839e24f78d64
Philipp <philipp@f1ndus.de>
2020-09-04 20:49:39 +0200

    added script

---
+++ init.ps1
@@ -0,0 +1,22 @@
+function InstallWSL {
+	dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+	dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+	wsl --set-default-version 2
+}
+
+function InstallChoco {
+	Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
+
+function InstallApps {
+	choco install packages.config --yes
+}
+
+function WindowsSetupScript {
+	./reclaim_win.cmd
+}
+
+#InstallChoco
+#InstallWSL
+#InstallApps
+WindowsSetupScript
